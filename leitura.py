13c56878f4f8eeb53949a3b8ed5c006a38b5943c
LeonardokSS <leonardoken02@gmail.com>
2025-07-29 12:07:23 -0300

    leitura

--- leitura.py
+++ leitura.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 Dicionário = {
  "Nomes": ["Ana", "Bruno", "Carlos"],
@@ -10,6 +12,7 @@ di = pd.DataFrame(Dicionário)
 print (di)
 
 #Lê o arquivo Excel e carrega a tabela em um DataFrame
+print("EXEL")
 ex = pd.read_excel('cardapio_restaurante.xlsx', sheet_name='Cardápio')
 print(ex)
 
@@ -18,12 +21,47 @@ Bc = pd.read_csv('funcionarios.csv')
 print(Bc)
 
 #Leitura de JSON e conversão para DataFrame
-Js = pd.read_json('pratos.json')
-print(Js)
+Js = pd.read_json("pratos.json")  
+
 
 # Leitura dos 5 primeiros e últimos registros de um DataFrame
 #Isso tranforma o dicionário em um DataFrame
-df = pd.DataFrame(Dicionário)
 
+df = pd.DataFrame(Dicionário)
+print("PRIMEIROS")
 df.head()
+print("Últimos")
 df.tail()
+
+#Leitura simples de itens de um DataFrame
+print(df["Nomes"])
+print(df["Idades"])
+
+#Leitura pela posição 
+#DataFrame.iloc[], Esse método seleciona colunas e linhas por numeros inteiros começando pelo 0
+df2 = pd.DataFrame({
+    'nome': ['Ana', 'Bruno', 'Carlos'],
+    'idade': [23, 35, 30],
+    'cidade': ['SP', 'RJ', 'BH']
+})
+
+#print(variavel.iloc[linha,coluna])
+#O slice serve para pegar uma parte do DataFrame
+# Pega a linha 0 e coluna 1 (idade de Ana)
+print(df2.iloc[0, 1])  # Saída: 23
+
+# Pega todas as linhas, coluna 2 (cidade)
+print(df2.iloc[:, 2])
+
+# Pega linhas 0 a 1 e colunas 0 a 1
+print(df2.iloc[0:2, 0:2])
+
+
+#o pandas ve o dicionario assim:
+#| índice | nome   | idade | cidade |
+#| ------ | --0--- | --1-- | --2--- |
+#| 0      | Ana    | 23    | SP     |
+#| 1      | Bruno  | 35    | RJ     |
+#| 2      | Carlos | 30    | BH     |
+
+
