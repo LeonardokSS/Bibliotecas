6433a834b39769273d1a937ba9bc6ce4b1499ac9
LeonardoKaprio <leonardoken02@gmail.com>
2025-08-15 13:16:04 -0300

    Add files via upload

---
+++ Teclado/Digitação.py
@@ -0,0 +1,8 @@
+import keyboard
+
+#Para que o código pressione as teclas para você, basta: keyboard.send('tecla')
+keyboard.send('a')
+
+
+#Para pressionar uma sequência de teclas basta 
+keyboard.press_and_release('ctrl+s')
