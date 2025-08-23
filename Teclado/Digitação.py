3a73f6abea434b1c4e67bc1744a225ef9ee4650f
LeonardoKaprio <leonardoken02@gmail.com>
2025-08-23 18:45:20 -0300

    Todos os comandos

--- Teclado/Digitação.py
+++ Teclado/Digitação.py
@@ -3,6 +3,15 @@ import keyboard
 #Para que o código pressione as teclas para você, basta: keyboard.send('tecla')
 keyboard.send('a')
 
+#Escreve a palavra a abaixo
+keyboard.write('palavra')
+
+#Segura a tecla alt
+keyboard.press('alt')
+
+#Solta a tecla alt
+keyboard.release('alt')
 
 #Para pressionar uma sequência de teclas basta 
 keyboard.press_and_release('ctrl+s')
+
